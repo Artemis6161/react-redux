88080eeab2981f2822407bed1dc807c126584ec2
sangeetha <Artemis6161@github.com>
2022-06-08 21:13:10 +0530

    store change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Header from './containers/header'
+import Productlisting from './containers/productlisting';
+import Productdetails from './containers/productdetails';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+     <Header />
+     <Routes>
+     <Route path="/" element={<Productlisting/>} />
+     <Route path="/product/:productId" element={<Productdetails/>} />
+     <Route> 404 Not Found!</Route>
+     </Routes>
+     </Router>
+</div>
   );
 }
 
